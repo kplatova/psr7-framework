da873379696f2ae9bc63dedafc727ade25dc5548
Ксения Платова <kseniya.platova@iss.digital>
2017-12-19 23:06:55 +0300

    Подключаю пакет по проверке безопасных расширений и обновляю файл composer.json

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,16 @@
 <?php
+
+use Framework\Http\Request;
+
+chdir(dirname(__DIR__));
+require 'src/Framework/Http/Request.php';
+
+### Initialization
+
+$request = new Request();
+
+### Action
+
 $name = $_GET['name'] ?? 'Guest';
 header('X-Developer: Kplatova');
 echo 'Hello, ' . $name . '!' . PHP_EOL;
---
+++ src/Framework/Http/Request.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Framework\Http;
+
+class Request
+{
+    public function getQueryParams(): array
+    {
+        return $_GET;
+    }
+    public function getParsedBody(): array
+    {
+        return $_POST ?: null;
+    }
+}
